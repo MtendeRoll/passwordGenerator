ecde8fd0d3950119855c9a5715a78d9ee5abeaec
Roll Hub <rollmtende@gmail.com>
2022-05-08 16:54:01 -0400

    console log each prompt to make sure it worked

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ var haveNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var storedOptions = [];
 
 // prompt for length of password between 8-128
-var promptLength = function () {
+function promptLength() {
   var passwordLength = window.prompt("How long would you like your password to be? Choose betwen 8 and 128 Characters");
 
   // ensuring a valid input
@@ -18,59 +18,69 @@ var promptLength = function () {
 
   passwordLength = parseInt(passwordLength);
   console.log(passwordLength);
-};
+  promptSpecial();
+}
 
 // prompt for special characters
-var promptSpecial = function () {
+function promptSpecial() {
   var confirmSpecial = window.confirm("Would you like your password to have a special character? e.g (.!?/");
 
   // ensure valid input
   if (confirmSpecial) {
     // push into empty array
-    storedOptions.push(haveCharacters);
+    storedOptions = storedOptions.concat(haveCharacters);
+    console.log(storedOptions);
   }
-};
+
+  promptLowerCase();
+}
 // prompt for lower case
-var promptLowerCase = function () {
+function promptLowerCase() {
   var confirmLowerCase = window.confirm("Would you like your password to have a lower case?");
 
   // ensure valid input
   if (confirmLowerCase) {
     // push into empty array
-    storedOptions.push(haveLowerCase);
+    storedOptions = storedOptions.concat(haveLowerCase);
   }
-};
+
+  promptUpperCase();
+}
 // prompt for upper case
-var promptUpperCase = function () {
+function promptUpperCase() {
   var confirmUpperCase = window.confirm("Would you like your password to have a upper case?");
 
   // ensure valid input
   if (confirmUpperCase === true) {
     // pick a special character
-    randomIndex(haveUpperCase);
+    storedOptions = storedOptions.concat(haveUpperCase);
   }
-};
+
+  promptNumbers();
+}
 //prompt for numbers
-var promptNumbers = function () {
+function promptNumbers() {
   var confirmNumbers = window.confirm("Would you like your password to have a number?");
 
   // ensure valid input
   if (confirmNumbers) {
     // push into empty array
-    storedOptions.push(haveNumbers);
+    storedOptions = storedOptions.concat(haveNumbers);
   }
-};
+}
 
 // make generate password button clickable
 var generatePassword = function () {
   promptLength();
-  console.log(promptLength);
+  console.log(storedOptions);
 
-  for (var i = 0; i < promptLength; i++) {
-    var randomIndex = function (array) {
-      return Math.floor(Math.random() * promptLength);
-    };
-  }
+  var newPassword = [];
+
+  // for (var i = 0; i < promptLength; i++) {
+  //   var randomIndex = function (array) {
+  //     return Math.floor(Math.random() * promptLength);
+  //   };
+  // }
 };
 
 // Get references to the #generate element
